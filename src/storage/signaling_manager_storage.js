3ae8cc76dc536583c60c06adb51ebf48f90ec19a
saudsami <saudsami@gmail.com>
2023-07-07 09:19:22 +0500

    Updates

--- src/storage/signaling_manager_storage.js
+++ src/storage/signaling_manager_storage.js
@@ -15,75 +15,95 @@ const SignalingManagerStorage = async (messageCallback, eventsCallback) => {
   };
 
   const setUserMetadata = async function (uid, key, value) {
+    // Define a data array to hold key-value pairs
     const data = [
       {
-        key: key,
-        value: value,
-        revision: -1, 
+        key: key,  // Metadata Item's key
+        value: value,  // Metadata Item's value
+        revision: -1, // Versioning switch on write operations:
+        // -1: Turn off version checking.
+        // > 0, The target revision number must match this value for the operation to succeed.
       },
     ];
 
     const options = {
-      userId: uid,
-      majorRevision: -1,
-      addTimeStamp: true,
-      addUserId: true,
+      userId: uid, // current user's uid
+      majorRevision: -1, // Version control switch, -1 => Turn off version checking
+      addTimeStamp: true, // Whether to record timestamps of edits
+      addUserId: true, // Whether to log the editor's user ID
     };
 
     try {
-      await signalingManager
-        .getSignalingEngine()
+      await signalingManager.getSignalingEngine()
         .storage.setUserMetadata(data, options);
       messageCallback(`User metadata key ${key} saved`);
     } catch (status) {
-      console.log(status);
+        console.log(status);
     }
   };
 
   const updateUserMetadata = async function (uid, key, value) {
+    // Define a data array to hold key-value pairs
     const data = [
       {
-        key: key,
-        value: value,
-        revision: -1,
+        key: key,  // Metadata Item's key
+        value: value,  // Metadata Item's value
+        revision: -1, // Versioning switch on write operations:
+        // -1: Turn off version checking
+        // > 0, The target revision number must match this value for the operation to succeed.
       },
     ];
 
     const options = {
-      userId: uid,
-      majorRevision: -1,
-      addTimeStamp: true,
-      addUserId: true,
+      userId: uid, // current user's uid
+      majorRevision: -1, // Version control switch, -1 => Turn off version checking
+      addTimeStamp: true, // Whether to record timestamps of edits
+      addUserId: true, // Whether to log the editor's user ID
     };
 
     try {
-      await signalingManager
-        .getSignalingEngine()
+      await signalingManager.getSignalingEngine()
         .storage.updateUserMetadata(data, options);
       messageCallback(`User metadata key ${key} saved`);
+    } catch (status) {
+        console.log(status);
+    }
+  };
+
+  const getUserMetadata = async function (uid) {
+    try {
+      const result = await signalingManager
+        .getSignalingEngine()
+        .storage.getUserMetadata({ userId: uid });
+      return result.metadata;
     } catch (status) {
       console.log(status);
     }
   };
 
   const setChannelMetadata = async function (channelName, key, value) {
-    const metaData = [
+    // Define a data array to hold key-value pairs
+    const data = [
       {
-        key: key,
-        value: value,
-        revision: -1,
+        key: key,  // Metadata Item's key
+        value: value,  // Metadata Item's value
+        revision: -1, // Versioning switch on write operations:
+        // -1: Turn off version checking.
+        // > 0, The target revision number must match this value for the operation to succeed.
       },
     ];
+
     const options = {
       majorRevision: -1,
-      lockName: "",
+      lockName: "", // After setting a lock, only the user who calls the acquireLock method to acquire the lock can perform the operation.
       addTimeStamp: true,
       addUserId: true,
     };
+
     try {
       const result = await signalingManager
         .getSignalingEngine()
-        .storage.setChannelMetadata(channelName, "MESSAGE", metaData, options);
+        .storage.setChannelMetadata(channelName, "MESSAGE", data, options);
       messageCallback(`channel metadata ${key} set successfully`);
     } catch (status) {
       console.log(status);
@@ -95,17 +115,6 @@ const SignalingManagerStorage = async (messageCallback, eventsCallback) => {
       const result = await signalingManager
         .getSignalingEngine()
         .storage.getChannelMetadata(channelName, channelType);
-      return result.metadata;
-    } catch (status) {
-      console.log(status);
-    }
-  };
-
-  const getUserMetadata = async function (uid) {
-    try {
-      const result = await signalingManager
-        .getSignalingEngine()
-        .storage.getUserMetadata({ userId: uid });
       return result.metadata;
     } catch (status) {
       console.log(status);
