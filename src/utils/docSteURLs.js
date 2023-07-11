9e1cb9713419483386d5158687bca58da6b55547
billy-the-fish <iain.cox@sarkdocumentation.com>
2023-07-11 09:35:04 +0200

    Update on review.

--- src/utils/docSteURLs.js
+++ src/utils/docSteURLs.js
@@ -1,11 +1,11 @@
 var docURLs = {
-  sdk_quickstart: "https://www.google.com",
-  authentication: "https://www.google.com",
-  stream: "https://www.google.com",
-  metadata: "https://www.google.com",
-  dataEncryption: "https://www.google.com",
-  geofencing: "https://www.google.com",
-  cloudproxy: "https://www.google.com",
+  sdk_quickstart: "https://docs-staging-git-milestone-22-signalling-211-agora-gdxe.vercel.app/en/signaling/get-started/get-started-sdk?platform=web",
+  authentication: "https://docs-staging-git-milestone-22-signalling-211-agora-gdxe.vercel.app/en/signaling/get-started/authentication-workflow?platform=web",
+  stream: "https://docs-staging-git-milestone-22-signalling-211-agora-gdxe.vercel.app/en/signaling/develop/stream-channel?platform=web",
+  metadata: "https://docs-staging-git-milestone-22-signalling-211-agora-gdxe.vercel.app/en/signaling/develop/storage?platform=web",
+  cloudproxy: "https://docs-staging-git-milestone-22-signalling-211-agora-gdxe.vercel.app/en/signaling/develop/cloud-proxy?platform=web",
+  dataEncryption: "https://docs-staging-git-milestone-22-signalling-211-agora-gdxe.vercel.app/en/signaling/develop/data-encryption?platform=web",
+  geofencing: "https://docs-staging-git-milestone-22-signalling-211-agora-gdxe.vercel.app/en/signaling/enable-features/geofencing?platform=web",
 };
 
 export default docURLs;
