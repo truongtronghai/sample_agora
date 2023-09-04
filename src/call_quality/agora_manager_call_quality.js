4b9a617db183ccfe3516cdc9df694a093978707d
Kishan Dhakan <kishandhakan17@gmail.com>
2023-09-04 14:59:36 +0100

    add call quality

---
+++ src/call_quality/agora_manager_call_quality.js
@@ -0,0 +1,143 @@
+import AgoraRTCManager from "../agora_manager/agora_manager.js";
+import AgoraRTC from "agora-rtc-sdk-ng";
+
+const AgoraRTCCallQuality = async (eventsCallback) => {
+  // Extend the AgoraManager by importing it
+  const agoraManager = await AgoraRTCManager(eventsCallback);
+  const agoraEngine = agoraManager.getAgoraEngine();
+
+  agoraManager
+    .getAgoraEngine()
+    .on("connection-state-change", (curState, prevState, reason) => {
+      eventsCallback("connection-state-change", curState, prevState, reason);
+    });
+
+  // Get the uplink network condition
+  agoraEngine.on("network-quality", (quality) => {
+    if (quality.uplinkNetworkQuality == 1) {
+      document.getElementById("upLinkIndicator").innerHTML = "Excellent";
+      document.getElementById("upLinkIndicator").style.color = "green";
+    } else if (quality.uplinkNetworkQuality == 2) {
+      document.getElementById("upLinkIndicator").innerHTML = "Good";
+      document.getElementById("upLinkIndicator").style.color = "yellow";
+    } else quality.uplinkNetworkQuality >= 4;
+    {
+      document.getElementById("upLinkIndicator").innerHTML = "Poor";
+      document.getElementById("upLinkIndicator").style.color = "red";
+    }
+  });
+
+  // Get the downlink network condition
+  agoraEngine.on("network-quality", (quality) => {
+    if (quality.downlinkNetworkQuality == 1) {
+      document.getElementById("downLinkIndicator").innerHTML = "Excellent";
+      document.getElementById("downLinkIndicator").style.color = "green";
+    } else if (quality.downlinkNetworkQuality == 2) {
+      document.getElementById("downLinkIndicator").innerHTML = "Good";
+      document.getElementById("downLinkIndicator").style.color = "yellow";
+    } else if (quality.downlinkNetworkQuality >= 4) {
+      document.getElementById("downLinkIndicator").innerHTML = "Poor";
+      document.getElementById("downLinkIndicator").style.color = "red";
+    }
+  });
+
+  const getDevices = async () => {
+    const devices = await AgoraRTC.getDevices();
+    const audioDevices = devices.filter(function (device) {
+      return device.kind === "audioinput";
+    });
+    const videoDevices = devices.filter(function (device) {
+      return device.kind === "videoinput";
+    });
+    return {
+      audioDevices,
+      videoDevices,
+    };
+  };
+
+  const createTestTracks = async (camera, mic) => {
+    const videoTrack = AgoraRTC.createCameraVideoTrack({
+      cameraId: camera,
+    });
+    const audioTrack = AgoraRTC.createMicrophoneAudioTrack({
+      microphoneId: mic,
+    });
+    return {
+      videoTrack,
+      audioTrack,
+    };
+  };
+
+  const setRemoteVideoStreamType = (remoteuid, quality) => {
+    agoraEngine.setRemoteVideoStreamType(remoteuid, quality);
+  };
+
+  const enableDualStreamMode = () => {
+    // Enable dual-stream mode.
+    agoraEngine.enableDualStream();
+  };
+
+  const setAudioProfile = async () => {
+    // Create a local audio track and set an audio profile for the local audio track.
+    channelParameters.localAudioTrack =
+      await AgoraRTC.createMicrophoneAudioTrack({
+        encoderConfig: "high_quality_stereo",
+      });
+  };
+
+  const setVideoProfile = async () => {
+    // Set a video profile.
+    channelParameters.localVideoTrack = await AgoraRTC.createCameraVideoTrack({
+      optimizationMode: "detail",
+      encoderConfig: {
+        width: 640,
+        // Specify a value range and an ideal value
+        height: { ideal: 480, min: 400, max: 500 },
+        frameRate: 15,
+        bitrateMin: 600,
+        bitrateMax: 1000,
+      },
+    });
+  };
+
+  const getStatistics = async (remoteUid) => {
+    const localAudioStats = agoraEngine.getLocalAudioStats();
+    const localVideoStats = agoraEngine.getLocalVideoStats();
+    let remoteAudioStats;
+    let remoteVideoStats;
+    if (remoteUid !== undefined) {
+      remoteAudioStats = agoraEngine.getRemoteAudioStats()[remoteUid];
+      remoteVideoStats = agoraEngine.getRemoteVideoStats()[remoteUid];
+    }
+    const rtcStats = agoraEngine.getRTCStats();
+    return {
+      localAudioStats,
+      localVideoStats,
+      remoteAudioStats,
+      remoteVideoStats,
+      rtcStats,
+    };
+  };
+
+  const setStreamFallbackStrategy = async () => {
+    // Set a stream fallback option to automatically switch remote video quality when network conditions degrade.
+    agoraManager
+      .getAgoraEngine()
+      .setStreamFallbackOption(channelParameters.remoteUid, 1);
+  };
+
+  // Return the extended agora manager
+  return {
+    ...agoraManager,
+    enableDualStreamMode,
+    setAudioProfile,
+    setVideoProfile,
+    setStreamFallbackStrategy,
+    getDevices,
+    setRemoteVideoStreamType,
+    getStatistics,
+    createTestTracks,
+  };
+};
+
+export default AgoraRTCCallQuality;
---
+++ src/call_quality/call_quality.js
@@ -0,0 +1,238 @@
+import AgoraRTCCallQuality from "../call_quality/agora_manager_call_quality.js";
+import showMessage from "../utils/showMessage.js";
+import setupProjectSelector from "../utils/setupProjectSelector.js";
+import docURLs from "../utils/docSteURLs.js";
+
+// A variable to track the state of remote video quality.
+var isHighRemoteVideoQuality = false;
+// A variable to track the state of device test.
+var isDeviceTestRunning = false;
+// Variables to hold the Audio/Video tracks for device testing.
+var testTracks;
+// A variable to reference the audio devices dropdown.
+var audioDevicesDropDown;
+// A variable to reference the video devices dropdown.
+var videoDevicesDropDown;
+
+let channelParameters = {
+  // A variable to hold a local audio track.
+  localAudioTrack: null,
+  // A variable to hold a local video track.
+  localVideoTrack: null,
+  // A variable to hold a remote audio track.
+  remoteAudioTrack: null,
+  // A variable to hold a remote video track.
+  remoteVideoTrack: null,
+  // A variable to hold the remote user id.s
+  remoteUid: "1",
+};
+
+// The following code is solely related to UI implementation and not Agora-specific code
+window.onload = async () => {
+  // Set the project selector
+  setupProjectSelector();
+
+  const handleVSDKEvents = (eventName, ...args) => {
+    switch (eventName) {
+      case "user-published":
+        if (args[1] == "video") {
+          // Retrieve the remote video track.
+          channelParameters.remoteVideoTrack = args[0].videoTrack;
+          // Retrieve the remote audio track.
+          channelParameters.remoteAudioTrack = args[0].audioTrack;
+          // Save the remote user id for reuse.
+          channelParameters.remoteUid = args[0].uid.toString();
+          // Specify the ID of the DIV container. You can use the uid of the remote user.
+          remotePlayerContainer.id = args[0].uid.toString();
+          channelParameters.remoteUid = args[0].uid.toString();
+          remotePlayerContainer.textContent =
+            "Remote user " + args[0].uid.toString();
+          // Append the remote container to the page body.
+          document.body.append(remotePlayerContainer);
+
+          // Change stream quality on click
+          document.getElementById(remotePlayerContainer.id).addEventListener("click", function () {
+            if (isHighRemoteVideoQuality === false) {
+              agoraManager.setRemoteVideoStreamType(channelParameters.remoteUid, 0);
+              isHighRemoteVideoQuality = true;
+            } else {
+              agoraManager.setRemoteVideoStreamType(channelParameters.remoteUid, 1);
+              isHighRemoteVideoQuality = false;
+            }
+          });
+          // Play the remote video track.
+          channelParameters.remoteVideoTrack.play(remotePlayerContainer);
+        }
+        // Subscribe and play the remote audio track If the remote user publishes the audio track only.
+        if (args[1] == "audio") {
+          // Get the RemoteAudioTrack object in the AgoraRTCRemoteUser object.
+          channelParameters.remoteAudioTrack = args[0].audioTrack;
+          // Play the remote audio track. No need to pass any DOM element.
+          channelParameters.remoteAudioTrack.play();
+        }
+      case "connection-state-change":
+        // The sample code uses debug console to show the connection state. In a real-world application, you can add
+        // a label or a icon to the user interface to show the connection state.
+
+        // Display the current connection state.
+        console.log("Connection state has changed to :" + args[0]);
+        // Display the previous connection state.
+        console.log("Connection state was : " + args[1]);
+        // Display the connection state change reason.
+        console.log("Connection state change reason : " + args[2]);
+    }
+  };
+
+  const agoraManager = await AgoraRTCCallQuality(handleVSDKEvents);
+
+  // Display channel name
+  document.getElementById("channelName").innerHTML =
+    agoraManager.config.channelName;
+  // Display User name
+  document.getElementById("userId").innerHTML = agoraManager.config.uid;
+
+  // Get an instance of the Agora Engine from the manager
+  const agoraEngine = await agoraManager.getAgoraEngine();
+
+  // Dynamically create a container in the form of a DIV element to play the remote video track.
+  const remotePlayerContainer = document.createElement("div");
+  // Dynamically create a container in the form of a DIV element to play the local video track.
+  const localPlayerContainer = document.createElement("div");
+  // Specify the ID of the DIV container. You can use the uid of the local user.
+  localPlayerContainer.id = agoraManager.config.uid;
+  remotePlayerContainer.id = "1"; // initialize, will be updated when user joins
+  // Set the textContent property of the local video container to the local user id.
+  localPlayerContainer.textContent = "Local user " + agoraManager.config.uid;
+  // Set the local video container size.
+  localPlayerContainer.style.width = "640px";
+  localPlayerContainer.style.height = "480px";
+  localPlayerContainer.style.padding = "15px 5px 5px 5px";
+  // Set the remote video container size.
+  remotePlayerContainer.style.width = "640px";
+  remotePlayerContainer.style.height = "480px";
+  remotePlayerContainer.style.padding = "15px 5px 5px 5px";
+
+  audioDevicesDropDown = document.getElementById("audioDevices");
+  videoDevicesDropDown = document.getElementById("videoDevices");
+  const devices = await agoraManager.getDevices();
+  for (let i = 0; i < devices.audioDevices.length; i++) {
+    var option = document.createElement("option");
+    option.text = devices.audioDevices[i].label;
+    option.value = devices.audioDevices[i].deviceId;
+    audioDevicesDropDown.appendChild(option);
+  }
+  for (let i = 0; i < devices.videoDevices.length; i++) {
+    var option = document.createElement("option");
+    option.text = devices.videoDevices[i].label;
+    option.value = devices.videoDevices[i].deviceId;
+    videoDevicesDropDown.appendChild(option);
+  }
+
+  // Listen to the Join button click event.
+  document.getElementById("join").onclick = async function () {
+    // Join a channel.
+    await agoraManager.join(localPlayerContainer, channelParameters);
+    console.log("publish success!");
+  };
+
+  // Listen to the Leave button click event.
+  document.getElementById("leave").onclick = async function () {
+    removeVideoDiv(remotePlayerContainer.id);
+    removeVideoDiv(localPlayerContainer.id);
+    // Leave the channel
+    await agoraManager.leave(channelParameters);
+    console.log("You left the channel");
+    // Refresh the page for reuse
+    window.location.reload();
+  };
+
+  document.getElementById("statistics").onclick = async function () {
+    // The sample code uses debug console to show the call-quality statistics. In a real-world application, you can
+    // add label or paragraph to the user interface to show the call-quality statistics.
+
+    const stats = await agoraManager.getStatistics(remotePlayerContainer.id);
+    console.log(stats);
+
+    console.log(
+      "Local audio stats = { sendBytes :" +
+        stats.localAudioStats.sendBytes +
+        ", sendBitrate :" +
+        stats.localAudioStats.sendBitrate +
+        ", sendPacketsLost :" +
+        stats.localAudioStats.sendPacketsLost +
+        " }"
+    );
+
+    console.log(
+      "Local video stats = { sendBytes :" +
+        stats.localVideoStats.sendBytes +
+        ", sendBitrate :" +
+        stats.localVideoStats.sendBitrate +
+        ", sendPacketsLost :" +
+        stats.localVideoStats.sendPacketsLost +
+        " }"
+    );
+
+    if (stats.remoteAudioStats !== undefined) {
+      console.log(
+        "Remote audio stats = { receiveBytes :" +
+          stats.remoteAudioStats.receivedBytes +
+          ", receiveBitrate :" +
+          stats.remoteAudioStats.receiveBitrate +
+          ", receivePacketsLost :" +
+          stats.remoteAudioStats.receivePacketsLost +
+          "}"
+      );
+    }
+
+    if (stats.remoteVideoStats !== undefined) {
+      console.log(
+        " Local video stats = { receiveBytes :" +
+          stats.remoteVideoStats.receiveBytes +
+          ", receiveBitrate :" +
+          stats.remoteVideoStats.receiveBitrate +
+          ", receivePacketsLost :" +
+          stats.remoteVideoStats.receivePacketsLost +
+          " }"
+      );
+    }
+
+    console.log(
+      "Channel statistics = { UserCount :" +
+        stats.rtcStats.UserCount +
+        ", OutgoingAvailableBandwidth :" +
+        stats.rtcStats.OutgoingAvailableBandwidth +
+        ", RTT :" +
+        stats.rtcStats.RTT +
+        " }"
+    );
+  };
+
+  document.getElementById("testDevices").onclick = async function () {
+    console.log(testTracks)
+    if (isDeviceTestRunning === false) {
+      testTracks = await agoraManager.createTestTracks(videoDevicesDropDown.value, audioDevicesDropDown.value)
+      document.body.append(localPlayerContainer);
+      (await testTracks.videoTrack).play(localPlayerContainer);
+      (await testTracks.audioTrack).play();
+      isDeviceTestRunning = true;
+      document.getElementById("testDevices").innerHTML = "Stop test";
+    } else {
+      document.getElementById("testDevices").innerHTML =
+        "Start audio/video device test";
+      isDeviceTestRunning = false;
+      (await testTracks.videoTrack).close();
+      (await testTracks.audioTrack).close();
+      let Div = document.getElementById(localPlayerContainer.id);
+      Div.remove();
+    }
+  };
+};
+
+function removeVideoDiv(elementId) {
+  console.log("Removing " + elementId + "Div");
+  let Div = document.getElementById(elementId);
+  if (Div) {
+    Div.remove();
+  }
+}
