75cc4225a4502609dab31de4493c77219ced8020
Kishan Dhakan <kishandhakan17@gmail.com>
2023-09-05 11:57:28 +0100

    add scure auth

---
+++ src/secure_authentication/agora_manager_secure_auth.js
@@ -0,0 +1,77 @@
+import AgoraManager from "../agora_manager/agora_manager.js";
+import AgoraRTC from "agora-rtc-sdk-ng";
+
+const AgoraManagerAuthentication = async (eventsCallback) => {
+  let streamChannel = null;
+  let role = "publisher"; // set the role to "publisher" or "subscriber" as appropriate
+
+  // Extend the AgoraManager by importing it
+  const agoraManager = await AgoraManager(eventsCallback);
+
+  // Get the config
+  const config = agoraManager.config;
+
+  // Fetches the RTC token for stream channels
+  async function fetchRTCToken(uid, channelName) {
+    if (config.serverUrl !== "") {
+      try {
+        const res = await fetch(
+          config.proxyUrl +
+            config.serverUrl +
+            "/rtc/" +
+            channelName +
+            "/" +
+            role +
+            "/uid/" +
+            uid +
+            "/?expiry=" +
+            config.tokenExpiryTime,
+          {
+            headers: {
+              "X-Requested-With": "XMLHttpRequest",
+            },
+          }
+        );
+        const data = await res.text();
+        const json = await JSON.parse(data);
+        console.log("RTC token fetched from server: ", json.rtcToken);
+        return json.rtcToken;
+      } catch (err) {
+        console.log(err);
+      }
+    } else {
+      return config.token;
+    }
+  }
+
+  const joinWithToken = async (localPlayerContainer, channelParameters) => {
+    const token = await fetchRTCToken(config.uid, config.channelName);
+    await agoraManager
+      .getAgoraEngine()
+      .join(config.appId, config.channelName, token, config.uid);
+    // Create a local audio track from the audio sampled by a microphone.
+    channelParameters.localAudioTrack =
+      await AgoraRTC.createMicrophoneAudioTrack();
+    // Create a local video track from the video captured by a camera.
+    channelParameters.localVideoTrack = await AgoraRTC.createCameraVideoTrack();
+    // Append the local video container to the page body.
+    document.body.append(localPlayerContainer);
+    // Publish the local audio and video tracks in the channel.
+    await agoraManager
+      .getAgoraEngine()
+      .publish([
+        channelParameters.localAudioTrack,
+        channelParameters.localVideoTrack,
+      ]);
+    // Play the local video track.
+    channelParameters.localVideoTrack.play(localPlayerContainer);
+  };
+
+  // Return the extended agora manager
+  return {
+    ...agoraManager,
+    joinWithToken,
+  };
+};
+
+export default AgoraManagerAuthentication;
